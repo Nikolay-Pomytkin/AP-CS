ef088e7e563bd3b95fc3cc9d7bee0deb0546c02c
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-09-21 08:48:01 -0400

    Update sortAll.java

--- sortAll.java
+++ sortAll.java
@@ -1,11 +1,4 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+public class sortAll
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -13,18 +6,12 @@ class Ideone
 	}
 	
 	public static Comparable [] bubbleSort (Comparable x []){
-    for(int i = 0; i < x.length(); i++){
-      for(int f = 0; f< x.length(); f++){
-		z = x[f].compareTo(x[f+1]);
-		if (z == -1){
-				
-		} else if (z == 0){
-				
-		} else if (z == 1){
-				
-		}
+	    for(int i = 0; i < x.length(); i++){
+	      for(int f = 0; f< x.length(); f++){
+			z = x[f].compareTo(x[f+1]);
+		
+	        }
+	    }
         }
-      }
-    }
-  }
 }
+
