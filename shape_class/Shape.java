4f985bd0ad14c0926c79da738b1d5184dd4d88f3
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-10-18 08:58:37 -0400

    Update Shape.java

--- shape_class/Shape.java
+++ shape_class/Shape.java
@@ -1,9 +1,31 @@
 package shape;
 
 public class Shape {
-  private double radius;
-  public Shape(n, c , r){
-    super(n, c);
-    radius = r;
+  private String name;
+  private String color;
+  
+  public Shape(n, c){
+    name = n;
+    color = c;
   }
+  
+  public Shape(){
+		name = "Shape";
+		color = "White";
+	}
+	
+	public abstract double calcArea;
+	public abstract String type;
+  
+	public String getName(){
+		return name;
+	}
+
+	public String getColor(){
+		return color;
+	}
+	
+	public String toString(){
+		return "Name: " + name + ", Type: " + type() + ", Color: " + getColor() + ", Area: " + calcArea();
+	}
 }
